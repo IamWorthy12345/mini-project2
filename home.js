eb3aa582c8c4eeba6cdb7ecfdb6e70bb5b0ca9ba
janela317 <castulojanela@gmail.com>
2023-03-13 02:52:38 +0800

    Interface Update

---
+++ home.js
@@ -0,0 +1,14 @@
+const BUTTON_BOOK = document.querySelector('#btn-show-tours');
+const USER_LOGGED = document.querySelector('#store-user');
+
+if (USER_LOGGED.textContent.endsWith('Logged In')){
+    BUTTON_BOOK.addEventListener('click', function(e){
+        e.preventDefault();
+        window.location.href = 'destinations.html';
+    });
+} else {
+    BUTTON_BOOK.addEventListener('click', function(e){
+        e.preventDefault();
+        window.location.href = 'signin.html';
+    });
+}
