f18aa8effed3ce23e260bcac1b48bc496f23f68a
janela317 <castulojanela@gmail.com>
2023-03-13 21:07:07 +0800

    UI update

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 const BUTTON_BOOK = document.querySelector('#btn-show-tours');
-const USER_LOGGED = document.querySelector('#store-user');
+const USER_LOGGED = document.querySelector('.stored-user');
 
 if (USER_LOGGED.textContent.endsWith('Logged In')){
     BUTTON_BOOK.addEventListener('click', function(e){
