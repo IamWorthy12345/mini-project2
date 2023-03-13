ee07a241c02cd0427d83b07d5cc5ac6719ab4de1
janela317 <castulojanela@gmail.com>
2023-03-13 19:25:29 +0800

    Update Book Tour Page

--- book-tour-script.js
+++ book-tour-script.js
@@ -1,69 +1,100 @@
-const YES_TOGGLER = document.querySelector("#yes-hotel");
-const NO_TOGGLER = document.querySelector("#no-hotel");
-const ACCOM = document.querySelector(".map-link");
+const OUT_DEST = localStorage.getItem("output_destination");
+document.getElementById("destination").value = OUT_DEST;
+const TDATE1 = localStorage.getItem("output_date1");
+document.getElementById("label-date1").textContent = TDATE1;
+const TDATE2 = localStorage.getItem("output_date2");
+document.getElementById("label-date2").textContent = TDATE2;
+const TDATE3 = localStorage.getItem("output_date3");
+document.getElementById("label-date3").textContent = TDATE3;
 
-YES_TOGGLER.addEventListener('click', function(e){
-    if (e.target){
-        ACCOM.style.display = "block";
+const PRICE_3D = localStorage.getItem("output_3d_price");
+const PRICE_2D = localStorage.getItem("output_2d_price");
+const RADIO_3D = document.querySelector("#three-days");
+const RADIO_2D = document.querySelector("#two-days");
+const TOUR_AMOUNT = document.querySelector("#amount");
+const labelDate1 = document.getElementById('date-1');
+const labelDate2 = document.getElementById('date-2');
+const labelDate3 = document.getElementById('date-3');
+const fromDate = document.getElementById('from-date');
+const toDate = document.getElementById('to-date');
+
+RADIO_3D.addEventListener('change', function(){
+    TOUR_AMOUNT.value = PRICE_3D;
+    if (labelDate1.checked){
+        const date = new Date(TDATE1);
+        fromDate.value = new Date(date.setDate(date.getDate() + 1)).toISOString().slice(0, 10);
+        toDate.value = new Date(date.setDate(date.getDate() + 3)).toISOString().slice(0, 10);
+    } else if (labelDate2.checked){
+        const date = new Date(TDATE2);
+        fromDate.value = new Date(date.setDate(date.getDate() + 1)).toISOString().slice(0, 10);
+        toDate.value = new Date(date.setDate(date.getDate() + 3)).toISOString().slice(0, 10);
+    } else if (labelDate3.checked){
+        const date = new Date(TDATE3);
+        fromDate.value = new Date(date.setDate(date.getDate() + 1)).toISOString().slice(0, 10);
+        toDate.value = new Date(date.setDate(date.getDate() + 3)).toISOString().slice(0, 10);
     }
 });
-NO_TOGGLER.addEventListener('click', function(e){
-    if (e.target){
-        ACCOM.style.display = "none";
+RADIO_2D.addEventListener('change', function(){
+    TOUR_AMOUNT.value = PRICE_2D;
+    if (labelDate1.checked){
+        const date = new Date(TDATE1);
+        fromDate.value = new Date(date.setDate(date.getDate() + 1)).toISOString().slice(0, 10);
+        toDate.value = new Date(date.setDate(date.getDate() + 2)).toISOString().slice(0, 10);
+    } else if (labelDate2.checked){
+        const date = new Date(TDATE2);
+        fromDate.value = new Date(date.setDate(date.getDate() + 1)).toISOString().slice(0, 10);
+        toDate.value = new Date(date.setDate(date.getDate() + 2)).toISOString().slice(0, 10);
+    } else if (labelDate3.checked){
+        const date = new Date(TDATE3);
+        fromDate.value = new Date(date.setDate(date.getDate() + 1)).toISOString().slice(0, 10);
+        toDate.value = new Date(date.setDate(date.getDate() + 2)).toISOString().slice(0, 10);
     }
 });
 
-const YES_MEAL_TOGGLER = document.querySelector("#yes-meal");
-const NO_MEAL_TOGGLER = document.querySelector("#no-meal");
-const MEALS = document.querySelector("#include-meals");
+labelDate1.addEventListener('change', function() {
+    const date = new Date(TDATE1);
+    fromDate.value = new Date(date.setDate(date.getDate() + 1)).toISOString().slice(0, 10);
+    if (RADIO_3D.checked){
+        toDate.value = new Date(date.setDate(date.getDate() + 3)).toISOString().slice(0, 10);
+    } else if (RADIO_2D.checked){
+        toDate.value = new Date(date.setDate(date.getDate() + 2)).toISOString().slice(0, 10);
+    } else {
+        toDate.value = new Date(date.setDate(date.getDate() + 3)).toISOString().slice(0, 10);
+    }
+});
 
-YES_MEAL_TOGGLER.addEventListener('click', function(e){
-    if (e.target){
-        MEALS.style.display = "flex";
+labelDate2.addEventListener('change', function() {
+    const date = new Date(TDATE2);
+    fromDate.value = new Date(date.setDate(date.getDate() + 1)).toISOString().slice(0, 10);
+    if (RADIO_3D.checked){
+        toDate.value = new Date(date.setDate(date.getDate() + 3)).toISOString().slice(0, 10);
+    } else if (RADIO_2D.checked){
+        toDate.value = new Date(date.setDate(date.getDate() + 2)).toISOString().slice(0, 10);
+    } else {
+        toDate.value = new Date(date.setDate(date.getDate() + 3)).toISOString().slice(0, 10);
     }
 });
-NO_MEAL_TOGGLER.addEventListener('click', function(e){
-    if (e.target){
-        MEALS.style.display = "none";
+
+labelDate3.addEventListener('change', function() {
+    const date = new Date(TDATE3);
+    fromDate.value = new Date(date.setDate(date.getDate() + 1)).toISOString().slice(0, 10);
+    if (RADIO_3D.checked){
+        toDate.value = new Date(date.setDate(date.getDate() + 3)).toISOString().slice(0, 10);
+    } else if (RADIO_2D.checked){
+        toDate.value = new Date(date.setDate(date.getDate() + 2)).toISOString().slice(0, 10);
+    } else {
+        toDate.value = new Date(date.setDate(date.getDate() + 3)).toISOString().slice(0, 10);
     }
 });
-const DIV_LIST = document.querySelector(".inclusion-list");
-let enterButton = document.getElementById("btn-enter");
-let input = document.getElementById("userInput");
-let ul = document.querySelector("#inclusion-ul");
-function inputLength() {
-    return input.value.length;
+
+const inclusions = localStorage.getItem("output_inclusions");
+const inclusionList = document.getElementById("inclusion-ul");
+if (inclusions) {
+    const inclusionsArray = JSON.parse(inclusions);
+
+    for (let i = 0; i < inclusionsArray.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = inclusionsArray[i];
+        inclusionList.appendChild(li);
+    }
 }
-function createListElement() {
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
-
-    let dBtn = document.createElement("button");
-    dBtn.appendChild(document.createTextNode("X"));
-    li.appendChild(dBtn);
-    dBtn.addEventListener("click", deleteListItem);
-  
-    function deleteListItem(event) {
-        event.preventDefault();
-        li.classList.add("delete");
-    }
-  }
-  function addListAfterClick(event) {
-    DIV_LIST.style.display = "block";
-    event.preventDefault();
-    if (inputLength() > 0) {
-      createListElement();
-    }
-  }
-  function addListAfterKeypress(event) {
-    if (inputLength() > 0 && event.which === 13) {
-        DIV_LIST.style.display = "block";
-        event.preventDefault();
-        createListElement();
-    }
-  }
-  enterButton.addEventListener("click", addListAfterClick);
-  input.addEventListener("keypress", addListAfterKeypress);
-
